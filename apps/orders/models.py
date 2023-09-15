5fa09c9bcb5304f669d34db1323003cdf5077e03
Oleh <ytox@ukr.net>
2023-09-16 00:07:49 +0300

    add endpoints to app orders

--- apps/orders/models.py
+++ apps/orders/models.py
@@ -5,7 +5,7 @@ class OrderModel(models.Model):
     name = models.CharField(max_length=20)
     surname = models.CharField(max_length=35)
     email = models.EmailField(max_length=254)
-    phone = models.IntegerField()
+    phone = models.BigIntegerField()
     age = models.IntegerField()
     course = models.CharField(max_length=5)
     course_format = models.CharField(max_length=10)
--- apps/orders/views.py
+++ apps/orders/views.py
@@ -1,22 +1,55 @@
+from django.http import Http404
+
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .models import OrderModel
+from .serializers import OrderSerializer
 
 
 class OrdersListCreateView(APIView):
-    def post(self, *args, **kwargs):
-        return Response('Method POST')
 
     def get(self, *args, **kwargs):
-        return Response('Method GET')
+        orders = OrderModel.objects.all()
+        serializer = OrderSerializer(orders, many=True)
+        return Response(serializer.data, status.HTTP_200_OK)
+
+    def post(self, *args, **kwargs):
+        data: dict = self.request.data
+        serializer = OrderSerializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status.HTTP_201_CREATED)
 
 
-class OrderView(APIView):
+class OrderRetrieveUpdateDestroyView(APIView):
     def get(self, *args, **kwargs):
         pk = kwargs['pk']
-        return Response(f'Method GET, order id: {pk}')
+        try:
+            order = OrderModel.objects.get(pk=pk)
+        except OrderModel.DoesNotExist:
+            raise Http404()
+        serializer = OrderSerializer(order)
+        return Response(serializer.data, status.HTTP_200_OK)
 
     def patch(self, *args, **kwargs):
         pk = kwargs['pk']
-        return Response(f'Method PATCH, order id: {pk}')
+        data: dict = self.request.data
+        try:
+            order = OrderModel.objects.get(pk=pk)
+        except OrderModel.DoesNotExist:
+            raise Http404()
+        serializer = OrderSerializer(order, data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status.HTTP_200_OK)
+
+    def delete(self, *args, **kwargs):
+        pk = kwargs['pk']
+        try:
+            order = OrderModel.objects.get(pk=pk)
+            order.delete()
+        except OrderModel.DoesNotExist:
+            raise Http404()
+        return Response('Deleted', status.HTTP_204_NO_CONTENT)
