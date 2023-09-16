5696239ef4d58cbae16b8b2ac078a91417902769
Oleh <ytox@ukr.net>
2023-09-16 17:46:15 +0300

    refactoring view orders

--- apps/orders/views.py
+++ apps/orders/views.py
@@ -1,15 +1,13 @@
-from django.http import Http404
-
 from rest_framework import status
+from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
-from rest_framework.views import APIView
 
 from .filters import order_filtered_queryset
 from .models import OrderModel
 from .serializers import OrderSerializer
 
 
-class OrdersListCreateView(APIView):
+class OrdersListCreateView(GenericAPIView):
     def get(self, *args, **kwargs):
         qs = order_filtered_queryset(self.request.query_params)
         serializer = OrderSerializer(qs, many=True)
@@ -23,33 +21,23 @@ class OrdersListCreateView(APIView):
         return Response(serializer.data, status.HTTP_201_CREATED)
 
 
-class OrderRetrieveUpdateDestroyView(APIView):
+class OrderRetrieveUpdateDestroyView(GenericAPIView):
+    serializer_class = OrderSerializer
+    queryset = OrderModel.objects.all()
+
     def get(self, *args, **kwargs):
-        pk = kwargs['pk']
-        try:
-            order = OrderModel.objects.get(pk=pk)
-        except OrderModel.DoesNotExist:
-            raise Http404()
-        serializer = OrderSerializer(order)
+        order = self.get_object()
+        serializer = self.get_serializer(order)
         return Response(serializer.data, status.HTTP_200_OK)
 
     def patch(self, *args, **kwargs):
-        pk = kwargs['pk']
         data: dict = self.request.data
-        try:
-            order = OrderModel.objects.get(pk=pk)
-        except OrderModel.DoesNotExist:
-            raise Http404()
+        order = self.get_object()
         serializer = OrderSerializer(order, data, partial=True)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status.HTTP_200_OK)
 
     def delete(self, *args, **kwargs):
-        pk = kwargs['pk']
-        try:
-            order = OrderModel.objects.get(pk=pk)
-            order.delete()
-        except OrderModel.DoesNotExist:
-            raise Http404()
+        self.get_object().delete()
         return Response('Deleted', status.HTTP_204_NO_CONTENT)
