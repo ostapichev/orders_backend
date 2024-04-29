80518daf6f33c5b966d15cb9bfde5a25d518cced
olehlevik <ytox@ukr.net>
2024-04-29 23:14:28 +0300

    fixed bag patch order

--- core/services/order_service.py
+++ core/services/order_service.py
@@ -4,7 +4,7 @@ from core.exception.order_permission_exception import OrderPermissionException
 class OrderService:
     @staticmethod
     def patch_method(request, order):
-        if request.user.id == order.manager_id:
+        if not order.manager_id or request.user.id == order.manager_id:
             if request.data['status'] == 'new_order':
                 order.manager_id = None
                 order.save()
