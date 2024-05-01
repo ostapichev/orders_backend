13db540174e5223cdfecbb715eb4d087cb22cc93
olehlevik <ytox@ukr.net>
2024-05-01 08:29:06 +0300

    postman file updated and fixed filters for users

--- apps/admin/filters.py
+++ apps/admin/filters.py
@@ -4,4 +4,4 @@ from django_filters import rest_framework as filters
 class UserFilter(filters.FilterSet):
     name = filters.CharFilter('profile__name', 'icontains')
     surname = filters.CharFilter('profile__surname', 'icontains')
-    emai = filters.CharFilter('email', 'icontains')
+    email = filters.CharFilter('email', 'icontains')
